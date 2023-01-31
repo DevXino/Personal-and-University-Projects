9cfd0b0812b1d17187be5233026e0c58bce4bfd1
DevXino <89163601+DevXino@users.noreply.github.com>
2023-01-31 12:40:28 +0100

    New Static Folder

---
+++ Easy Wander/WebApp/website/static/main.js
@@ -0,0 +1,54 @@
+/*Questa parte rappresenta lo script principale*/
+
+
+/*Sezione per la gestione delle pagine della nav bar, precisamente si occupa di gestire gli
+* eventi nel momento in cui andiamo a selezionare ad esempio lo scan o la maps*/
+window.onload = () => {
+    const tab_switchers = document.querySelectorAll('[data-switcher]');
+
+    for (let i = 0; i < tab_switchers.length; i++) {
+        const tab_switcher = tab_switchers[i];
+        const page_id = tab_switcher.dataset.tab;
+
+        tab_switcher.addEventListener('click', () => {
+            document.querySelector('.tabs .tab.is-active').classList.remove('is-active');
+            tab_switcher.parentNode.classList.add('is-active');
+
+            SwitchPage(page_id);
+        });
+    }
+}
+
+function SwitchPage (page_id) {
+    console.log(page_id);
+
+    const current_page = document.querySelector('.pages .page.is-active');
+    current_page.classList.remove('is-active');
+
+    const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+    next_page.classList.add('is-active');
+}
+
+/*Sezione che gestisce il QR-code*/
+function onScanSuccess(qrCodeMessage) {
+    document.getElementById('result').innerHTML = '<span class="result">'+qrCodeMessage+'</span>';
+}
+function onScanError(errorMessage) {
+    console.warn(`Code scan error = ${error}`);
+}
+var html5QrcodeScanner = new Html5QrcodeScanner(
+    "qr-reader", { fps: 10, qrbox: 250 });
+html5QrcodeScanner.render(onScanSuccess, onScanError);
+
+
+async function registerSW() {
+  if ('serviceWorker' in navigator) {
+    try {
+      await navigator.serviceWorker.register('./sw.js');
+    } catch (e) {
+      console.log(`SW registration failed`);
+    }
+  }
+}
+
+
